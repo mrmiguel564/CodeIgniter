c7ea1f58d95653a418eafdc6fc09c27f71d029f2
mrmiguel564 <miguel.espinoza@alumnos.uv.cl>
2019-07-14 21:33:47 -0400

    test 2

--- application/views/login.php
+++ application/views/login.php
@@ -82,14 +82,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   </div>
 
   <!-- Bootstrap core JavaScript-->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/jquery-easing/jquery.easing.min.js"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src="js/sb-admin-2.min.js"></script>
+  <script src="assets/js/sb-admin-2.min.js"></script>
 
 </body>
 
