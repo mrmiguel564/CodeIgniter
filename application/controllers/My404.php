3115eee7814db18d7c6be2c689503231962b6c24
mrmiguel564 <miguel.espinoza@alumnos.uv.cl>
2019-07-15 19:49:01 -0400

    estoy entrando en panico

--- application/controllers/My404.php
+++ application/controllers/My404.php
@@ -1,14 +1,13 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-class My404 extends CI_Controller
-{
-   public function __construct()
-   {
-       parent::__construct();
+
+class My404 extends CI_Controller{
+    function __construct(){
+		parent::__construct();
    }
-   public function index()
-   {
-       $this-<output-<set_status_header('404');
-       $this-<load-<view('err404');    
+   public function index(){
+   		$dato1['message'] = 'weaaaaa';
+   		$dato2['heading'] = 'weaasdaaaaa';
+       	$this->load->view('error_404',$dato1,$dato2);    
   }
 }
