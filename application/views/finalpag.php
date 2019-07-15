6f28acc13a2c554449f7484aa3b9da7d2506177c
mrmiguel564 <miguel.espinoza@alumnos.uv.cl>
2019-07-14 22:15:37 -0400

    borrando weas

--- application/views/finalpag.php
+++ application/views/finalpag.php
@@ -53,14 +53,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src="js/sb-admin-2.min.js"></script>
+  <script src="assets/js/sb-admin-2.min.js"></script>
 
   <!-- Page level plugins -->
-  <script src="vendor/chart.js/Chart.min.js"></script>
+  <script src="assets/vendor/chart.js/Chart.min.js"></script>
 
   <!-- Page level custom scripts -->
-  <script src="js/demo/chart-area-demo.js"></script>
-  <script src="js/demo/chart-pie-demo.js"></script>
+  <script src="assets/js/demo/chart-area-demo.js"></script>
+  <script src="assets/js/demo/chart-pie-demo.js"></script>
 
 </body>
 
