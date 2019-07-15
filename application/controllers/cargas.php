0661225474e05e91c7721f35d690be1b9b05fe20
mrmiguel564 <miguel.espinoza@alumnos.uv.cl>
2019-07-14 23:11:44 -0400

    agregando pagina(del prototypo original)

--- application/controllers/cargas.php
+++ application/controllers/cargas.php
@@ -12,8 +12,8 @@ class Cargas extends CI_Controller {
 		$this->load->view('cabezapag'); 
 		$this->load->view('sidebar');	// modificar para que con una variable poder selecionar que opcion esta marcada
 		$this->load->view('topbar');
-		$data['titulo'] = 'titulo'
-		$this->load->view('subtitulo',$data);	
+		$dato['titulo'] = 'weaaaaa'; // remplazar por lo retornado por la base de datos
+		$this->load->view('subtitulo',$dato);	
 		/* comienzo cuerpo*/
 
 		$this->load->view('card');	
